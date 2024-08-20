c3d745e353ef8e09b76529d4f91371f63149073b
gf <gfjz666@163.com>
2024-08-20 17:20:00 +0800

    commit

---
+++ log.h
@@ -0,0 +1,11 @@
+#pragma once
+include <mutex>
+#include <string>
+#include <thread>
+#include <sys/time.h>
+#include <string.h>
+#include <stdarg.h>           // vastart va_end
+#include <assert.h>
+#include <sys/stat.h>         // mkdir
+#include "blockqueue.h"
+#include "../buffer/buffer.h"
