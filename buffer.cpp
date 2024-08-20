c3717e94fc7256dd85e0e26d895ddb6578c64e4f
gf <gfjz666@163.com>
2024-08-20 11:43:24 +0800

    buffer

--- buffer.cpp
+++ buffer.cpp
@@ -40,30 +40,144 @@ void buffer::EnsureWritable(size_t len)
 
 void buffer::HasWritten(size_t len)
 {
+    // write指针向后移动
+    writepos += len;
 }
 
 void buffer::Retrieve(size_t len)
 {
+    // read指针向后移动
+    readpos += len;
 }
 
 void buffer::RetrieveUntil(const char *end)
 {
+    // read指针移动到end
+    assert(Peek() <= end);
+    Retrieve(end - Peek());
+}
+
+void buffer::RetrieveAll()
+{
+    // 取出所有数据，buffer归零，读写下标归零,在别的函数中会用到
+    // bzero() 能够将内存块（字符串）的前n个字节清零
+    bzero(&buffer_[0], buffer_.size());
+    readpos = 0;
+    writepos = 0;
+}
+
+std::string buffer::RetrieveAllTostr()
+{
+    // 取出剩余可读的str
+    std::string str(Peek(), ReadableBytes());
+    RetrieveAll();
+    return str;
+}
+
+const char *buffer::BeginWriteConst() const
+{
+    // 写指针的位置
+    return &buffer_[writepos];
+}
+
+char *buffer::BeginWrite()
+{
+    return &buffer_[writepos];
+}
+
+// 添加str到缓冲区
+
+void buffer::append(const char *str, size_t len)
+{
+    assert(str);
+    EnsureWritable(len);
+    std::copy(str, str + len, BeginWrite); // 将str放到写下标开始的地方
+    HasWritten(len);                       // 移动写下标
+}
+
+void buffer::append(const std::string &str)
+{
+    append(str.c_str(), str.size());
+}
+
+void buffer::append(const void *data, size_t len)
+{
+    append(static_cast<const char *>(data), len);
+}
+
+void buffer::append(const buffer &buff)
+{
+    append(buff.Peek(), buff.ReadableBytes());
+}
+
+long buffer::ReadFd(int fd, int *Errno)
+{
+    char buff[65535]; // 栈区
+    struct iovec iov[2];
+    size_t writeable = WritableBytes(); // 先记录能写多少
+    // 分散读， 保证数据全部读完
+    iov[0].iov_base = BeginWrite();
+    iov[0].iov_len = writeable;
+    iov[1].iov_base = buff;
+    iov[1].iov_len = sizeof(buff);
+
+    long len = readv(fd, iov, 2);
+    if (len < 0)
+    {
+        *Errno = errno;
+    }
+    else if (static_cast<size_t>(len) <= writeable)
+    {                    // 若len小于writable，说明写区可以容纳len
+        writepos += len; // 直接移动写下标
+    }
+    else
+    {
+        writepos = buffer_.size();                          // 写区写满了,下标移到最后
+        append(buff, static_cast<size_t>(len - writeable)); // 剩余的长度
+    }
+    return len;
+}
+
+long buffer::WriteFd(int fd, int *Errno)
+{
+    size_t readSize = ReadableBytes();
+    long len = write(fd, Peek(), readSize);
+    if (len < 0)
+    {
+        *Errno = errno;
+        return len;
+    }
+    Errno += len;
+    return len;
 }
 
 const char *buffer::Beginptr() const
 {
+    // 缓冲区头部地址
     return &buffer_[0];
 }
 
 char *buffer::BeginPtr_()
 {
-    //缓冲区头部地址
+    // 缓冲区头部地址
     return &buffer_[0];
 }
 
 void buffer::MakeSpace_(size_t len)
 {
-    //实现空间扩容
-    if()
-
+    // 实现空间扩容
+    if (WritableBytes() + PreendableBytes() < len)
+    {
+        // 实际可用空间小于需要写入的空间
+        buffer_.resize(writepos + len + 1);
+    }
+    else
+    {
+        // 内存挪动
+        size_t readable = ReadableBytes();
+        std::copy(Beginptr() + readpos, Beginptr() + writepos, Beginptr());
+        readpos = 0;
+        writepos = readable;
+        assert(readable == ReadableBytes());
+    }
 }
--- buffer.h
+++ buffer.h
@@ -22,23 +22,35 @@ public:
     buffer(int initBuffsize = 1024);
     ~buffer() = default;
 
+    //缓冲区可以读写、预留空间的字节数
     size_t WritableBytes() const;
     size_t ReadableBytes() const;
     size_t PreendableBytes() const;
 
+    //得到预留空间的尾指针
     const char *Peek() const;
+    //确保可写，否则扩容
     void EnsureWritable(size_t len); 
     void HasWritten(size_t len);
 
     void Retrieve(size_t len);
     void RetrieveUntil(const char *end);
 
+    void RetrieveAll();
+    std::string RetrieveAllTostr();
+
     const char* BeginWriteConst() const;
     char* BeginWrite();
 
+    // 向缓冲区写入数据,重载append
     void append(const std::string &str);
     void append(const char*str, size_t len);
+    void append(const void *data,size_t len);
+    void append(const buffer &buff);
     
-
+    // 从socket通信的对端，接收数据到缓冲区
+    long ReadFd(int fd,int*Errno);
+    // 将缓冲区中的数据，发送到socket通信的对端
+    long WriteFd(int fd,int*Errno);
 
 };
