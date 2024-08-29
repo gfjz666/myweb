c24db114760c5b6e97450f3cb5e067252810e1fb
gf <gfjz666@163.com>
2024-08-29 20:35:01 +0800

    8.29

--- httpconn.h
+++ httpconn.h
@@ -21,5 +21,19 @@ public:
     ssize_t readBuffer(int* saveErrno);
     ssize_t writeBuffer(int* saveErrno);
 
+    //关闭HTTP连接的接口
+    void closeHTTPConn();
+    //定义处理该HTTP连接的接口，主要分为request的解析和response的生成
+    bool handleHTTPConn();
+
+    //其他方法
+    const char* getIP() const;
+    int getPort() const;
+    int getFd() const;
+    sockaddr_in getAddr() const;
+
+    int writebytes(){ return iov_[1].iov_len+iov_[0].iov_len;}
     
+private:
+
 };
